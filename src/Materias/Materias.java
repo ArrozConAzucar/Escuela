89ba759a8baaca8a6e8aea575e54534cd8eb9c58
ArrozConAzucar <Welcz_Enzo@hotmail.com>
2018-08-10 09:20:25 -0300

    Materias

---
+++ src/Materias/Materias.java
@@ -0,0 +1,21 @@
+package Materias;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+@Entity
+public class Materias {
+	
+	@Id @Column (length = 20) @Required
+	private String materia;
+
+	public String getMateria() {
+		return materia;
+	}
+
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
+
+}
